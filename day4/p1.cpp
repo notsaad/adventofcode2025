e87e94b95bfaf8172f72863793bb84f510653cdd
notsaad <saadmazharr@gmail.com>
2026-01-09 17:39:30 -0500

    code reviewed for best practices
    Signed-off-by: notsaad <saadmazharr@gmail.com>

--- day4/p1.cpp
+++ day4/p1.cpp
@@ -9,44 +9,29 @@ int main()
     while (getline(std::cin, line))
     {
         std::vector<int> row;
-        for (size_t i = 0; i < line.size(); ++i)
-        {
-            if (line[i] == '.')
-            {
-                row.push_back(0);
-            }
-            else
-            {
-                row.push_back(1);
-            }
+        for (char c : line) {
+            row.push_back(c == '.' ? 0 : 1);
         }
         grid.push_back(row);
     }
 
-    const size_t ROWS = grid.size();
-    const size_t COLS = grid[0].size();
+    const int ROWS = grid.size();
+    const int COLS = grid[0].size();
     const std::vector<std::pair<int, int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {-1, 1}, {-1, -1}, {1, -1}};
 
     auto valid = [&grid, ROWS, COLS](int r, int c)
     {
-        if (r >= 0 && c >= 0 && r < ROWS && c < COLS && grid[r][c] == 1)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
+        return r >= 0 && c >= 0 && r < ROWS && c < COLS && grid[r][c] == 1;
     };
 
-    for (size_t r = 0; r < ROWS; ++r)
+    for (int r = 0; r < ROWS; ++r)
     {
-        for (size_t c = 0; c < COLS; ++c)
+        for (int c = 0; c < COLS; ++c)
         {
             if (grid[r][c] == 1)
             {
                 int n = 0;
-                for (auto dir : directions)
+                for (const auto& dir : directions)
                 {
                     int newR = r + dir.first;
                     int newC = c + dir.second;
@@ -61,5 +46,5 @@ int main()
         }
     }
 
-    std::cout << ans << "\n";
+    std::cout << ans << '\n';
 }
