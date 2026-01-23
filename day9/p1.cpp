50fd7daa9e0cd89c6bc2f51ef33c8e9783320b3e
notsaad <saadmazharr@gmail.com>
2026-01-23 15:24:10 -0500

    day 9 part 1
    Signed-off-by: notsaad <saadmazharr@gmail.com>

---
+++ day9/p1.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+struct Point {
+  long long x;
+  long long y;
+};
+
+int main() {
+  std::ifstream inputFile("input.txt");
+  long long ans = 0;
+  std::vector<Point> points;
+  std::string line;
+
+  while (getline(inputFile, line)) {
+    std::stringstream ss(line);
+    Point v;
+    char comma;
+
+    ss >> v.x >> comma >> v.y;
+    points.push_back(v);
+  }
+
+  for (size_t i=0; i<points.size(); ++i) {
+    for (size_t j=i+1; j<points.size(); ++j) {
+      Point a = points[i];
+      Point b = points[j];
+
+      long long area = ((std::llabs(a.x - b.x) +1) * (std::llabs(a.y - b.y) +1));
+
+      ans = std::max(ans, area);
+    }
+  }
+
+
+  std::cout << ans << '\n';
+}
